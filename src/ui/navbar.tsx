6b0bd065463f9c89e437e6760f3e3795292ea034
Javier Pardo <antonioj_2803@hotmail.com>
2024-01-23 23:35:10 -0500

    Added few changes

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -1,12 +1,16 @@
-import styles from "./navbar.module.css";
 import NavLinks from "./navlinks";
+import Button from "./button";
+
 import { roboto } from "./fonts";
+
+import styles from "./navbar.module.css";
 
 export default function Navbar() {
   return (
     <nav className={`${styles.mainNavbar} ${roboto.variable}`}>
       <div className={styles.mainNavbar__container}>
         <NavLinks />
+        <Button />
       </div>
     </nav>
   );
--- src/ui/navlinks.tsx
+++ src/ui/navlinks.tsx
@@ -1,4 +1,3 @@
-import Button from "./button";
 import styles from "./navbar.module.css";
 
 export default function NavLinks() {
@@ -26,7 +25,6 @@ export default function NavLinks() {
           </a>
         </li>
       </ul>
-      <Button />
     </>
   );
 }
