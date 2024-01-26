da13173993d9f5c03d31c1b3cbc37f5446785e4e
Javier Pardo <antonioj_2803@hotmail.com>
2024-01-25 23:54:00 -0500

    Almost finished grid

---
+++ src/ui/grid-items/skills.tsx
@@ -0,0 +1,5 @@
+import styles from "@/ui/grid-items/grid-item.module.css";
+
+export default function Skills() {
+  return <div className={`${styles.gridItem} ${styles.skills}`}>Skills</div>;
+}
--- src/ui/grid/grid.tsx
+++ src/ui/grid/grid.tsx
@@ -2,6 +2,8 @@ import styles from "@/ui/grid/grid.module.css";
 import AboutMe from "../grid-items/aboutme";
 import ColorTheme from "../grid-items/color-theme";
 import MyLocation from "../grid-items/myLocation";
+import Projects from "../grid-items/Projects";
+import Skills from "../grid-items/skills";
 
 export default function Grid() {
   return (
@@ -9,6 +11,8 @@ export default function Grid() {
       <AboutMe />
       <ColorTheme />
       <MyLocation />
+      <Projects />
+      <Skills />
     </div>
   );
 }
