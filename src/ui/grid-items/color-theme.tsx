1f6a67141172f8565331220ec1b19643363898c2
Javier Pardo <antonioj_2803@hotmail.com>
2024-01-26 10:52:30 -0500

    Working on Theme component

--- src/ui/grid-items/color-theme.tsx
+++ src/ui/grid-items/color-theme.tsx
@@ -1,5 +1,14 @@
 import styles from "@/ui/grid-items/grid-item.module.css";
 
 export default function ColorTheme() {
-  return <div className={`${styles.gridItem} ${styles.colorTheme}`}>Theme</div>;
+  return (
+    <div className={`${styles.gridItem} ${styles.colorTheme}`}>
+      <div className={`${styles.flexContainer}`}>
+        <div className={`${styles.colorTheme__header}`}>
+          <h3>Theme</h3>
+        </div>
+        <div className={`${styles.colorTheme__content}`}>aawd</div>
+      </div>
+    </div>
+  );
 }
