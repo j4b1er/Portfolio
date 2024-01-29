fe90ade6337aca32960e79d16d2c4ea727f00ec7
Javier Pardo <antonioj_2803@hotmail.com>
2024-01-29 16:24:38 -0500

    Weather component almost finished only missing effects

--- src/ui/grid-items/aboutme.tsx
+++ src/ui/grid-items/aboutme.tsx
@@ -1,5 +1,5 @@
 import styles from "@/ui/grid-items/grid-item.module.css";
 
 export default function AboutMe() {
-  return <div className={`${styles.gridItem} ${styles.aboutMe}`}>About me</div>;
+  return <div className={`${styles.gridItem} ${styles.aboutMe}`}></div>;
 }
--- src/ui/grid-items/myLocation.tsx
+++ src/ui/grid-items/myLocation.tsx
@@ -2,15 +2,77 @@ import styles from "@/ui/grid-items/grid-item.module.css";
 
 const url = `https://api.openweathermap.org/data/2.5/weather?zip=20886,us&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
 
-async function getData() {
+const weatherConditions = [
+  {
+    iconday: "01d",
+    iconnight: "01n",
+    sunmoon: true,
+    pos: "",
+  },
+  {
+    iconday: "02d",
+    iconnight: "02n",
+    sunmoon: true,
+    pos: "-100px",
+  },
+  {
+    iconday: "03d",
+    iconnight: "03n",
+    sunmoon: true,
+    pos: "-40px",
+  },
+  {
+    iconday: "04d",
+    iconnight: "04n",
+    sunmoon: false,
+    pos: "-10px",
+  },
+  {
+    iconday: "09d",
+    iconnight: "09n",
+    sunmoon: false,
+    pos: "-10px",
+  },
+  {
+    iconday: "10d",
+    iconnight: "10n",
+    sunmoon: true,
+    pos: "-40px",
+  },
+  {
+    iconday: "11d",
+    iconnight: "11n",
+    sunmoon: false,
+    pos: "-10px",
+  },
+  {
+    iconday: "13d",
+    iconnight: "13n",
+    sunmoon: false,
+    pos: "-10px",
+  },
+  {
+    iconday: "50d",
+    iconnight: "50n",
+    sunmoon: false,
+    pos: "-10px",
+  },
+];
+
+async function getWeatherData() {
   const res = await fetch(url, { next: { revalidate: 5 } });
   return res.json();
 }
 
 export default async function MyLocation() {
-  const { weather, main, clouds, sys, name } = await getData();
-  // const dayNight = weather[0].icon.split("")[2] === "d" ? "day" : "night";
-  const dayNight = "night";
+  const { weather, main, wind, clouds, sys, name } = await getWeatherData();
+  const dayNight = weather[0].icon.split("")[2] === "d" ? "day" : "night";
+  // const dayNight = "night";
+  const { sunmoon, pos } = weatherConditions.filter(
+    (condition) =>
+      condition.iconday === weather[0].icon ||
+      condition.iconnight === weather[0].icon
+  )[0];
 
   return (
     <div
@@ -38,13 +100,38 @@ export default async function MyLocation() {
             <p>{Math.round(main.feels_like)}°</p>
             <p>Feels like</p>
           </div>
+          {/* <div className={styles.myLocation__extraInfo___details}>
+            <p>{Math.round(main.humidity)}%</p>
+            <p>Humidity</p>
+          </div> */}
           <div className={styles.myLocation__extraInfo___details}>
-            <p>{`${Math.round(main.temp_min)}° | ${Math.round(
-              main.temp_max
-            )}°`}</p>
-            <p>Low High</p>
+            <p>{Math.round(wind.speed)} MPH</p>
+            <p>Wind</p>
           </div>
         </div>
+      </div>
+      {sunmoon && <div className={styles.myLocation__MoonSun}></div>}
+      <div className={styles.myLocation__LeftCloud} style={{ left: pos }}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+          <defs>
+            <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+              <stop
+                className={styles.myLocation__LeftCloud___topGradient}
+                offset="60%"
+              />
+              <stop
+                className={styles.myLocation__LeftCloud___bottomGradient}
+                offset="100%"
+              />
+            </linearGradient>
+          </defs>
+          <path d="M0 336c0 79.5 64.5 144 144 144H512c70.7 0 128-57.3 128-128c0-61.9-44-113.6-102.4-125.4c4.1-10.7 6.4-22.4 6.4-34.6c0-53-43-96-96-96c-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32C167.6 32 96 103.6 96 192c0 2.7 .1 5.4 .2 8.1C40.2 219.8 0 273.2 0 336z" />
+        </svg>
+      </div>
+      <div className={styles.myLocation__RightCloud} style={{ right: pos }}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+          <path d="M0 336c0 79.5 64.5 144 144 144H512c70.7 0 128-57.3 128-128c0-61.9-44-113.6-102.4-125.4c4.1-10.7 6.4-22.4 6.4-34.6c0-53-43-96-96-96c-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32C167.6 32 96 103.6 96 192c0 2.7 .1 5.4 .2 8.1C40.2 219.8 0 273.2 0 336z" />
+        </svg>
       </div>
     </div>
   );
