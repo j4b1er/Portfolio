74578a96676af81cda435ae41edd7e8b97ed32fd
Javier Pardo <antonioj_2803@hotmail.com>
2024-02-06 22:34:05 -0500

    Added hover effects and colors to projects

--- src/ui/grid-items/project.tsx
+++ src/ui/grid-items/project.tsx
@@ -51,7 +51,7 @@ export default function Project({ ProjectData }: ProjectProps) {
               <path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z" />
             </svg>
           </div>
-          <h3 className="text-black">Project</h3>
+          <h3 className={cardHover ? "text-black" : ""}>Project</h3>
           <AngleButton hoverStatus={cardHover} />
         </div>
         <div className={`${styles.projects__content}`}>
